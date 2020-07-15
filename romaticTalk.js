69e74a71b2e4db861a78e2516f0acc720981d4d2
lumdzeehol <lumdzeehol@outlook.com>
2020-07-15 22:57:18 +0800

    feat: update time

--- romaticTalk.js
+++ romaticTalk.js
@@ -7,21 +7,19 @@ class RomaticTalk {
   constructor() {
     this._updateTime = null;
     this._talk = null;
-    this._isToday = false;
   }
 
   isFresh() {
     if(!this._updateTime) return false;
     var to = new Date();
     var toDay = `${to.getFullYear()}-${to.getMonth()}-${to.getDate()}`
-    return this._isToday !== toDay;
+    return this._updateTime !== toDay;
   }
 
   setTalk(talk) {
     var current = new Date();
     var currentDay = `${current.getFullYear()}-${current.getMonth()}-${current.getDate()}`
     this._updateTime = currentDay;
-    console.log(this._updateTime);
     this._talk = talk;
   }
 
